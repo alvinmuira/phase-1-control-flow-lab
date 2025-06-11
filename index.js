14d88a3ab1b3bbe5d14054b8ddc4a7908e0ddcce
Alvin Muira <alvinmuira@gmail.com>
2025-06-11 18:29:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  let feet;
+  let scuberGreetingForFeet ;
+  if  (feet <= 400) {
+    scuberGreetingForFeet= 'This one is on me!';
+  } else if (feet <= 2000) {
+    scuberGreetingForFeet = 'That will be twenty bucks.';
+  } else if (feet>2500) {
+    scberGreetingForFeet ='No can do.';
+  } else {
+    console.log ('I will gladly take your thirty bucks.');
+  }
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  let city;
+  let ternaryCheckCity;
+  if (city === 'NYC') {
+    ternaryCheckCity = 'Ok, sounds good.';
+  } else {
+    console.log('No go.');
+  }
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  let tip;
+  let switchOnCharmFromTip;
+  if (tip==='generous') {
+    switchOnCharmFromTip = 'Thank you so much.';
+  } else if (tip==='not as generous') {
+    switchOnCharmFromTip = 'Thank you.';
+  } else if (tip ==='thanks for everything') {
+    switchOnCharmFromTip = 'Bye.';
+  }
 }
